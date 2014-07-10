ceb27593426b6e120920da35ca5c9a92d378ab2f
element94 <altkinx@gmail.com>
2014-07-10 16:46:46 -0700

    testing out automatic change tracking...
    let’s see how well this works.

--- test.java
+++ test.java
@@ -9,6 +9,9 @@ System.out.println("The test class has loaded!!! Git is working?");
 
 System.out.println("This is step 2. HA!!!!");
 
+System.out.print("Is automatic trakcing working>>>>????");
+
+System.out.println("Even more changes?????");
 
 }
 
