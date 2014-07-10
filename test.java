deac5ca85fd09ed960bf5a85b0e22e4994537ebc
element94 <altkinx@gmail.com>
2014-07-10 16:41:38 -0700

    anothe rcommit punks....

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class test {
+
+static {
+//a static block that executes upon loading.
+
+System.out.println("The test class has loaded!!! Git is working?");
+
+
+}
+
+
+}
