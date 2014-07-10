55cd1aa1991d3c7703754ecb0afacd636f76a1ad
element94 <altkinx@gmail.com>
2014-07-10 16:54:17 -0700

    added another file
    yes.

---
+++ test2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class test2 {
+
+  // Given a day number in 2008, an integer between 1 and 366,
+  // as a command-line argument, prints the date in month/day format.
+  // Example:
+  //	java DateConverter 365
+  // should print
+  //	 12/30
+
+  // The code is missing two assignment statements.
+  public static void main (String [ ] args) {
+   
+    test testObject = new test();
+
+  }
+}
